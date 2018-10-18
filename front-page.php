07cb91c7d874db4799439725d7dc41b3027a3201
unohanazuki <nagai.uzuki0001@gmail.com>
2018-10-18 10:18:45 +0900

    first commit

---
+++ front-page.php
@@ -0,0 +1,17 @@
+﻿<?php
+/**
+ * The template for displaying the front page.
+ *
+ * This is the template that displays on the front page only.
+ *
+ * @package Atemina
+ */
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
